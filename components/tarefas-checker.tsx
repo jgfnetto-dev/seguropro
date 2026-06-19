4afd60d67b369e3d0764307ced6149a02e119d1b
Jose Gomes <jgfnetto@gmail.com>
2026-06-19 19:45:13 -0300

    Poll for pending tarefas reminders every 60s while the app is open, not just on navigation

--- components/tarefas-checker.tsx
+++ components/tarefas-checker.tsx
@@ -2,17 +2,25 @@
 import { useEffect } from 'react'
 import { usePathname, useRouter } from 'next/navigation'
 
+const INTERVALO_MS = 60_000
+
 export function TarefasChecker() {
   const pathname = usePathname()
   const router = useRouter()
 
   useEffect(() => {
-    fetch('/api/tarefas/verificar-pendentes', { method: 'POST' })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data?.sent > 0) router.refresh()
-      })
-      .catch(() => {})
+    function verificar() {
+      fetch('/api/tarefas/verificar-pendentes', { method: 'POST' })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data?.sent > 0) router.refresh()
+        })
+        .catch(() => {})
+    }
+
+    verificar()
+    const id = setInterval(verificar, INTERVALO_MS)
+    return () => clearInterval(id)
   }, [pathname, router])
 
   return null
